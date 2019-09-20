c101f83e8f7bbee75fac6cbfaf39612a6beafedb
vsrk61 <rahul.kumar115@ad.infosys.com>
2019-09-20 15:33:32 +0530

    Enhanced UX on the Enable Location

--- public/js/app.js
+++ public/js/app.js
@@ -4,54 +4,54 @@ const message1 = document.querySelector('#message1')
 const message2 = document.querySelector('#message2')
 const autoDetect = document.querySelector('#radio')
 
-weatherForm.addEventListener('submit' , (e) => {
+weatherForm.addEventListener('submit', (e) => {
 
-    e.preventDefault()
-    const location = search.value;
+  e.preventDefault()
+  const location = search.value;
 
-    message1.textContent = 'Fetching data...'
-    message2.textContent = ''
+  message1.textContent = 'Fetching data...'
+  message2.textContent = ''
 
-    fetch('/weather?address='+location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
-        if(data.error){
-            message1.textContent = data.error
-        }
-        else{
-            message1.textContent = data.place
-            message2.textContent = data.forecast
-        }
+      if (data.error) {
+        message1.textContent = data.error
+      }
+      else {
+        message1.textContent = data.place
+        message2.textContent = data.forecast
+      }
     })
-})
+  })
 
 })
 
 autoDetect.addEventListener('click', (e) => {
-    e.preventDefault()
-    navigator.geolocation.getCurrentPosition(position => {
-        
-        const latitude = position.coords.latitude
-        const longitude = position.coords.longitude
+  navigator.geolocation.getCurrentPosition(position => {
 
-        message1.textContent = 'Fetching data...'
-        message2.textContent = ''
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
 
-        const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    message1.textContent = 'Fetching data...'
+    message2.textContent = ''
 
-        fetch(proxyUrl+'https://api.darksky.net/forecast/7de64b2b3eefdd7043982ca1e88d63dc/'+latitude+','+longitude).then((response) => {
-            response.json().then((data) => {
-                if(data.error){
-                    message1.textContent = data.error
-                }
-                else{
-                    fetch(proxyUrl+'https://us1.locationiq.com/v1/reverse.php?key=59511a0f3dcb43&lat='+latitude+'&lon='+longitude+'&format=json').then((response) => {
-                        response.json().then((data) => {
-                            message1.textContent = data.address.county
-                        })
-                    })
-                    message2.textContent = data.daily.summary + ' It is currently '+data.currently.temperature+' degrees out. The high temperature is '+ data.daily.data[0].temperatureHigh+ ' and the low temperature is '+ data.daily.data[0].temperatureLow+'. There is a ' + data.currently.precipProbability+'% chance of rain.'
-                }
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+
+    fetch(proxyUrl + 'https://api.darksky.net/forecast/7de64b2b3eefdd7043982ca1e88d63dc/' + latitude + ',' + longitude).then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          message1.textContent = data.error
+        }
+        else {
+          fetch(proxyUrl + 'https://us1.locationiq.com/v1/reverse.php?key=59511a0f3dcb43&lat=' + latitude + '&lon=' + longitude + '&format=json').then((response) => {
+            response.json().then(({ address: { county } }) => {
+              message1.textContent = county;
+              search.value = county;
             })
-        })
-    })    
+          })
+          message2.textContent = data.daily.summary + ' It is currently ' + data.currently.temperature + ' degrees out. The high temperature is ' + data.daily.data[0].temperatureHigh + ' and the low temperature is ' + data.daily.data[0].temperatureLow + '. There is a ' + data.currently.precipProbability + '% chance of rain.'
+        }
+      })
+    })
+  })
 })
